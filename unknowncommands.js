d82293f754d6e19023b0143b434f7fdfaa4510e3
raidencool <44273162+raidencool@users.noreply.github.com>
2018-10-18 21:22:35 +0300

    Delete unknowncommands.js

--- unknowncommands.js
+++
@@ -1,6 +0,0 @@
-const client = new commando.Client({
-    commandPrefix: '<Insert Your Prefix Here>',
-    owner: '<Insert Your User ID Here>',
-    disableEveryone: true,
-    unknownCommandResponse: false
-});
